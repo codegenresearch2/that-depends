c6ed2ca0ba43bdb9a98c2ef6d90ab99a842d9112
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-08 16:24:50 +0000

    Reset to 8adebe7cc47f409e4ed34bb380bb23af801abc3a and made changes for LLM Round 1

--- that_depends/providers/factories.py
+++ that_depends/providers/factories.py
@@ -15,7 +15,7 @@ class Factory(AbstractFactory[T_co]):
         self._kwargs: typing.Final = kwargs
 
     async def async_resolve(self) -> T_co:
-        if self._override is not None:
+        if self._override:
             return typing.cast(T_co, self._override)
 
         return self._factory(
@@ -24,7 +24,7 @@ class Factory(AbstractFactory[T_co]):
         )
 
     def sync_resolve(self) -> T_co:
-        if self._override is not None:
+        if self._override:
             return typing.cast(T_co, self._override)
 
         return self._factory(
@@ -42,7 +42,7 @@ class AsyncFactory(AbstractFactory[T_co]):
         self._kwargs: typing.Final = kwargs
 
     async def async_resolve(self) -> T_co:
-        if self._override is not None:
+        if self._override:
             return typing.cast(T_co, self._override)
 
         return await self._factory(
