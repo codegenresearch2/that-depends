d25d234c9b49c46c664e17c8700a2b5e3d698b41
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-08 16:49:34 +0000

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/providers/__init__.py
+++ that_depends/providers/__init__.py
@@ -6,6 +6,7 @@ from that_depends.providers.object import Object
 from that_depends.providers.resources import AsyncResource, Resource
 from that_depends.providers.selector import Selector
 from that_depends.providers.singleton import Singleton
+from that_depends.providers.attr_getter import AttrGetter
 from that_depends.container import container_context, fetch_context_item, sync_container_context
 
 __all__ = [
@@ -22,6 +23,7 @@ __all__ = [
     'Resource',
     'Selector',
     'Singleton',
+    'AttrGetter',
     'container_context',
     'fetch_context_item',
     'sync_container_context'
