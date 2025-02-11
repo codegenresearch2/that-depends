f641f040df9f920e9d52162d5ed86ee460a3cde4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:59:27 +0000

    Reset to 78a6202972062258c1bba568782c841f32c56a6c and made changes for LLM Round 1

--- that_depends/providers/resources.py
+++ that_depends/providers/resources.py
@@ -4,11 +4,11 @@ import warnings
 from that_depends.providers.base import AbstractResource, ResourceContext
 
 
-T_co = typing.TypeVar("T_co", covariant=True)
+T = typing.TypeVar("T")
 P = typing.ParamSpec("P")
 
 
-class Resource(AbstractResource[T_co]):
+class Resource(AbstractResource[T]):
     __slots__ = (
         "_is_async",
         "_creator",
@@ -20,24 +20,24 @@ class Resource(AbstractResource[T_co]):
 
     def __init__(
         self,
-        creator: typing.Callable[P, typing.Iterator[T_co] | typing.AsyncIterator[T_co]],
+        creator: typing.Callable[P, typing.Iterator[T] | typing.AsyncIterator[T]],
         *args: P.args,
         **kwargs: P.kwargs,
     ) -> None:
         super().__init__(creator, *args, **kwargs)
-        self._context: typing.Final[ResourceContext[T_co]] = ResourceContext(is_async=self._is_async)
+        self._context: typing.Final[ResourceContext[T]] = ResourceContext(is_async=self._is_async)
 
-    def _fetch_context(self) -> ResourceContext[T_co]:
+    def _fetch_context(self) -> ResourceContext[T]:
         return self._context
 
     async def tear_down(self) -> None:
         await self._fetch_context().tear_down()
 
 
-class AsyncResource(Resource[T_co]):
+class AsyncResource(Resource[T]):
     def __init__(
         self,
-        creator: typing.Callable[P, typing.AsyncIterator[T_co]],
+        creator: typing.Callable[P, typing.AsyncIterator[T]],
         *args: P.args,
         **kwargs: P.kwargs,
     ) -> None:
