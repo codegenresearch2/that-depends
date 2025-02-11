fcf739c0585ac09e1f1c51168fa538594a927136
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:47:58 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -1,6 +1,7 @@
 import typing
 
 from that_depends.providers.base import AbstractProvider
+from that_depends.providers.singleton import Singleton
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
@@ -15,9 +16,35 @@ class Object(AbstractProvider[T_co]):
         self._obj: typing.Final = obj
 
     async def async_resolve(self) -> T_co:
-        return self.sync_resolve()
+        return self._obj
+
+    def sync_resolve(self) -> T_co:
+        return self._obj
+
+
+class EnhancedObject(Singleton[T_co, P]):
+    __slots__ = ("_obj",)
+
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
+        super().__init__(factory, *args, **kwargs)
+        self._obj: T_co | None = None
+
+    async def async_resolve(self) -> T_co:
+        if self._override is not None:
+            return typing.cast(T_co, self._override)
+
+        if self._obj is not None:
+            return self._obj
+
+        async with self._resolving_lock:
+            if self._obj is None:
+                self._obj = await super().async_resolve()
+            return self._obj
 
     def sync_resolve(self) -> T_co:
         if self._override is not None:
             return typing.cast(T_co, self._override)
+
+        if self._obj is None:
+            self._obj = super().sync_resolve()
         return self._obj
