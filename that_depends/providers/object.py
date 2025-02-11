a8d9faa0a87c9439d00dab059b9bb1ee44d18cd4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:13:04 +0000

    Reset to 78a6202972062258c1bba568782c841f32c56a6c and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -3,19 +3,18 @@ import typing
 from that_depends.providers.base import AbstractProvider
 
 
-T = typing.TypeVar("T")
-P = typing.ParamSpec("P")
+T_co = typing.TypeVar("T_co", covariant=True)
 
 
-class Object(AbstractProvider[T]):
+class Object(AbstractProvider[T_co]):
     __slots__ = ("_obj",)
 
-    def __init__(self, obj: T) -> None:
+    def __init__(self, obj: T_co) -> None:
         super().__init__()
         self._obj: typing.Final = obj
 
-    async def async_resolve(self) -> T:
+    async def async_resolve(self) -> T_co:
         return self._obj
 
-    def sync_resolve(self) -> T:
+    def sync_resolve(self) -> T_co:
         return self._obj
