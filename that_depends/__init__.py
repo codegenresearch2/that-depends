cdf802b2dfc1a390e4721c0d3728877894b01681
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 21:21:37 +0000

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -1,15 +1,27 @@
-from that_depends.providers import container_context, fetch_context_item, sync_container_context
+from that_depends import providers
 from that_depends.container import BaseContainer
 from that_depends.injection import Provide, inject
+from that_depends.providers import container_context, fetch_context_item, sync_container_context
+
+
+class AsyncContextManager:
+    async def __aenter__(self):
+        # Context manager setup code
+        pass
+
+    async def __aexit__(self, exc_type, exc, tb):
+        # Context manager teardown code
+        pass
 
 
 async def container_context(context=None):
-    async with container_context(context) as context_manager:
+    async with AsyncContextManager() as context_manager:
         yield context_manager
     await sync_container_context()
 
 
 __all__ = [
+    "providers",
     "container_context",
     "fetch_context_item",
     "sync_container_context",
