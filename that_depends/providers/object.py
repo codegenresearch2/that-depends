c244551731127a464238c78c4407de813631b555
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:13:42 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -8,29 +8,28 @@ T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
 
 
-class ObjectProvider(AbstractProvider[T_co]):
-    __slots__ = ("_obj",)
-
-    def __init__(self, obj: T_co) -> None:
-        super().__init__()
-        self._obj: typing.Final = obj
-
-    async def async_resolve(self) -> T_co:
-        return self._obj
-
-    def sync_resolve(self) -> T_co:
-        return self._obj
-
-
 class Object(AbstractProvider[T_co]):
-    __slots__ = ("_provider",)
+    __slots__ = ("_provider", "_override")
 
     def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
         super().__init__()
         self._provider = Singleton(factory, *args, **kwargs)
+        self._override = None
 
     async def async_resolve(self) -> T_co:
         return await self._provider.async_resolve()
 
     def sync_resolve(self) -> T_co:
         return self._provider.sync_resolve()
+
+    def override(self, obj: T_co) -> None:
+        self._override = obj
+
+    async def async_override(self, obj: T_co) -> None:
+        self._override = obj
+
+    def clear_override(self) -> None:
+        self._override = None
+
+    async def async_clear_override(self) -> None:
+        self._override = None
