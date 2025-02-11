df9a078a116a225e36a434647aa763a2b888eaaf
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:47:48 +0000

    Reset to 630733ae0183f9ba7fad987b2bdbd275c23b506e and made changes for LLM Round 1

--- that_depends/providers/singleton.py
+++ that_depends/providers/singleton.py
@@ -1,6 +1,7 @@
 import asyncio
 import typing
 
+from that_depends.providers import AttrGetter
 from that_depends.providers.base import AbstractProvider
 
 
@@ -16,25 +17,24 @@ class Singleton(AbstractProvider[T_co]):
         self._factory: typing.Final = factory
         self._args: typing.Final = args
         self._kwargs: typing.Final = kwargs
+        self._override = None
         self._instance: T_co | None = None
         self._resolving_lock: typing.Final = asyncio.Lock()
+
+    def __getattr__(self, attr_name: str) -> typing.Any:  # noqa: ANN401
+        if attr_name.startswith("_"):
+            raise AttributeError(f"'{type(self)}' object has no attribute '{attr_name}'")
+        return AttrGetter(provider=self, attr_name=attr_name)
 
     async def async_resolve(self) -> T_co:
         if self._override is not None:
             return typing.cast(T_co, self._override)
 
-        if self._instance is not None:
-            return self._instance
-
-        # lock to prevent resolving several times
         async with self._resolving_lock:
             if self._instance is None:
-                self._instance = self._factory(
+                self._instance = await self._factory(
                     *[await x.async_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
-                    **{
-                        k: await v.async_resolve() if isinstance(v, AbstractProvider) else v
-                        for k, v in self._kwargs.items()
-                    },
+                    **{k: await v.async_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()},
                 )
             return self._instance
 
