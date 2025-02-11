01d9f1010bd935b94a7bb156f870f0e5a3a88d71
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 21:22:10 +0000

    Reset to 630733ae0183f9ba7fad987b2bdbd275c23b506e and made changes for LLM Round 1

--- that_depends/providers/collections.py
+++ that_depends/providers/collections.py
@@ -15,7 +15,7 @@ class List(AbstractProvider[list[T_co]]):
         self._providers: typing.Final = providers
 
     async def async_resolve(self) -> list[T_co]:
-        return await asyncio.gather(*[provider.async_resolve() for provider in self._providers])
+        return [await provider.async_resolve() for provider in self._providers]
 
     def sync_resolve(self) -> list[T_co]:
         return [provider.sync_resolve() for provider in self._providers]
