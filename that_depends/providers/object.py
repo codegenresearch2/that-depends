9856b336e0d9cea55242a53eca6161c546bc48d9
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:00:21 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -1,13 +1,14 @@
 import typing
 
 from that_depends.providers.base import AbstractProvider
+from that_depends.providers.singleton import Singleton
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
 
 
-class Object(AbstractProvider[T_co]):
+class ObjectProvider(AbstractProvider[T_co]):
     __slots__ = ("_obj",)
 
     def __init__(self, obj: T_co) -> None:
@@ -15,9 +16,21 @@ class Object(AbstractProvider[T_co]):
         self._obj: typing.Final = obj
 
     async def async_resolve(self) -> T_co:
-        return self.sync_resolve()
+        return self._obj
 
     def sync_resolve(self) -> T_co:
-        if self._override is not None:
-            return typing.cast(T_co, self._override)
         return self._obj
+
+
+class Object(AbstractProvider[T_co]):
+    __slots__ = ("_provider",)
+
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
+        super().__init__()
+        self._provider = Singleton(factory, *args, **kwargs)
+
+    async def async_resolve(self) -> T_co:
+        return await self._provider.async_resolve()
+
+    def sync_resolve(self) -> T_co:
+        return self._provider.sync_resolve()
