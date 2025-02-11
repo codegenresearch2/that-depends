f23a893d634e4164baf60869e8be90029798d086
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:39:34 +0000

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/__init__.py
+++ that_depends/__init__.py
@@ -1,6 +1,6 @@
 from that_depends.providers import container_context, fetch_context_item, providers, sync_container_context
 from that_depends.container import BaseContainer
-from that_depends.injection import inject, Provide
+from that_depends.injection import Provide, inject
 
 __all__ = [
     "container_context",
