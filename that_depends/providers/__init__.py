9445a15e05a6743c03a828744c328bb247afb94e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 20:13:19 +0000

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/providers/__init__.py
+++ that_depends/providers/__init__.py
@@ -1,29 +1,22 @@
-from that_depends.providers import (
-    AbstractProvider,
+from that_depends.providers.base import AbstractProvider
+from that_depends.providers.context_resources import (
     AsyncContextResource,
-    AsyncFactory,
-    AsyncResource,
-    AttrGetter,
     ContextResource,
     DIContextMiddleware,
-    Dict,
-    Factory,
-    List,
-    Object,
-    Resource,
-    Selector,
-    Singleton,
-    container_context,
-    fetch_context_item,
-    sync_container_context,
 )
+from that_depends.providers.factories import AsyncFactory, Factory
+from that_depends.providers.collections import Dict, List
+from that_depends.providers.object import Object
+from that_depends.providers.resources import AsyncResource, Resource
+from that_depends.providers.selector import Selector
+from that_depends.providers.singleton import Singleton
+from that_depends.providers import container_context
 
 __all__ = [
     "AbstractProvider",
     "AsyncContextResource",
     "AsyncFactory",
     "AsyncResource",
-    "AttrGetter",
     "ContextResource",
     "DIContextMiddleware",
     "Dict",
@@ -34,6 +27,4 @@ __all__ = [
     "Selector",
     "Singleton",
     "container_context",
-    "fetch_context_item",
-    "sync_container_context",
 ]
