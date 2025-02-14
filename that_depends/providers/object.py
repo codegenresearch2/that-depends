75c5c46464de3677e375f7b2bd7d5109baa367c6
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:40:40 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -1,13 +1,14 @@
 import typing
 
 from that_depends.providers.base import AbstractProvider
+from that_depends.providers.singleton import Singleton
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
 
 
-class Object(AbstractProvider[T_co]):
+class ObjectProvider(AbstractProvider[T_co]):
     __slots__ = ("_obj",)
 
     def __init__(self, obj: T_co) -> None:
@@ -15,9 +16,29 @@ class Object(AbstractProvider[T_co]):
         self._obj: typing.Final = obj
 
     async def async_resolve(self) -> T_co:
-        return self.sync_resolve()
+        return self._obj
 
     def sync_resolve(self) -> T_co:
-        if self._override is not None:
-            return typing.cast(T_co, self._override)
         return self._obj
+
+
+class ObjectFactory(AbstractProvider[T_co]):
+    __slots__ = ("_factory", "_args", "_kwargs")
+
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
+        super().__init__()
+        self._factory = factory
+        self._args = args
+        self._kwargs = kwargs
+
+    async def async_resolve(self) -> T_co:
+        return self._factory(
+            *[await x.async_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: await v.async_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()},
+        )
+
+    def sync_resolve(self) -> T_co:
+        return self._factory(
+            *[x.sync_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: v.sync_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()},
+        )
