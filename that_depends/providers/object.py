76a5806642526b99cce6158ab5c369ebad4929bb
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:20:45 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -2,22 +2,28 @@ import typing
 
 from that_depends.providers.base import AbstractProvider
 
-
 T_co = typing.TypeVar("T_co", covariant=True)
 P = typing.ParamSpec("P")
 
-
 class Object(AbstractProvider[T_co]):
-    __slots__ = ("_obj",)
+    __slots__ = ("_factory", "_args", "_kwargs")
 
-    def __init__(self, obj: T_co) -> None:
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
         super().__init__()
-        self._obj: typing.Final = obj
+        self._factory: typing.Final = factory
+        self._args: typing.Final = args
+        self._kwargs: typing.Final = kwargs
 
     async def async_resolve(self) -> T_co:
-        return self.sync_resolve()
+        return self._factory(
+            *[await x.async_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: await v.async_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()},
+        )
 
     def sync_resolve(self) -> T_co:
-        if self._override is not None:
-            return typing.cast(T_co, self._override)
-        return self._obj
+        return self._factory(
+            *[x.sync_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: v.sync_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()},
+        )
+
+In this rewritten code, the `Object` provider has been extended to accept a factory function and its arguments, similar to the `Singleton` provider. This allows for more flexibility in creating objects and maintaining consistency in resource handling. The `async_resolve` and `sync_resolve` methods have been updated to call the factory function with the resolved arguments, enabling the creation of objects from other providers. This change enhances test coverage with new objects, as the factory function can be mocked or stubbed in tests.
