2c7a7ab2383edc9ae891906d7004b3cefb2979c4
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:40:40 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- tests/container.py
+++ tests/container.py
@@ -66,4 +66,81 @@ class DIContainer(BaseContainer):
         async_resource=async_resource.cast,
     )
     singleton = providers.Singleton(SingletonFactory, dep1=True)
-    object = providers.Object(object())
+
+    @classmethod
+    def override_sync_resource(cls, new_resource: datetime.datetime) -> None:
+        cls.sync_resource.override(lambda: typing.cast(typing.Iterator[datetime.datetime], iter([new_resource])))
+
+    @classmethod
+    def override_async_resource(cls, new_resource: datetime.datetime) -> None:
+        cls.async_resource.override(lambda: typing.cast(typing.AsyncIterator[datetime.datetime], async_iter([new_resource])))
+
+    @classmethod
+    def override_simple_factory(cls, dep1: str, dep2: int) -> None:
+        cls.simple_factory.override(lambda: SimpleFactory(dep1=dep1, dep2=dep2))
+
+    @classmethod
+    def override_async_factory(cls, now: datetime.datetime) -> None:
+        cls.async_factory.override(lambda: async_factory(now))
+
+    @classmethod
+    def override_dependent_factory(cls, simple_factory: SimpleFactory, sync_resource: datetime.datetime, async_resource: datetime.datetime) -> None:
+        cls.dependent_factory.override(lambda: DependentFactory(simple_factory=simple_factory, sync_resource=sync_resource, async_resource=async_resource))
+
+    @classmethod
+    def override_singleton(cls, dep1: bool) -> None:
+        cls.singleton.override(lambda: SingletonFactory(dep1=dep1))
+
+    @classmethod
+    def get_sync_resource(cls) -> datetime.datetime:
+        return next(cls.sync_resource())
+
+    @classmethod
+    def get_async_resource(cls) -> datetime.datetime:
+        return next(cls.async_resource())
+
+    @classmethod
+    def get_simple_factory(cls) -> SimpleFactory:
+        return cls.simple_factory()
+
+    @classmethod
+    def get_async_factory(cls) -> datetime.datetime:
+        return cls.async_factory()
+
+    @classmethod
+    def get_dependent_factory(cls) -> DependentFactory:
+        return cls.dependent_factory()
+
+    @classmethod
+    def get_singleton(cls) -> SingletonFactory:
+        return cls.singleton()
+
+    @classmethod
+    def assert_sync_resource(cls) -> None:
+        resource = cls.get_sync_resource()
+        assert isinstance(resource, datetime.datetime)
+
+    @classmethod
+    def assert_async_resource(cls) -> None:
+        resource = cls.get_async_resource()
+        assert isinstance(resource, datetime.datetime)
+
+    @classmethod
+    def assert_simple_factory(cls) -> None:
+        factory = cls.get_simple_factory()
+        assert isinstance(factory, SimpleFactory)
+
+    @classmethod
+    def assert_async_factory(cls) -> None:
+        factory = cls.get_async_factory()
+        assert isinstance(factory, datetime.datetime)
+
+    @classmethod
+    def assert_dependent_factory(cls) -> None:
+        factory = cls.get_dependent_factory()
+        assert isinstance(factory, DependentFactory)
+
+    @classmethod
+    def assert_singleton(cls) -> None:
+        factory = cls.get_singleton()
+        assert isinstance(factory, SingletonFactory)
