5b4921722be95f36359db824f2b0e4e1b3a03ee8
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:23:18 +0000

    Reset to 630733ae0183f9ba7fad987b2bdbd275c23b506e and made changes for LLM Round 1

--- that_depends/providers/base.py
+++ that_depends/providers/base.py
@@ -4,7 +4,6 @@ import contextlib
 import inspect
 import typing
 from contextlib import contextmanager
-from operator import attrgetter
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
@@ -18,12 +17,6 @@ class AbstractProvider(typing.Generic[T_co], abc.ABC):
     def __init__(self) -> None:
         super().__init__()
         self._override: typing.Any = None
-
-    def __getattr__(self, attr_name: str) -> typing.Any:  # noqa: ANN401
-        if attr_name.startswith("_"):
-            msg = f"'{type(self)}' object has no attribute '{attr_name}'"
-            raise AttributeError(msg)
-        return AttrGetter(provider=self, attr_name=attr_name)
 
     @abc.abstractmethod
     async def async_resolve(self) -> T_co:
@@ -52,20 +45,7 @@ class AbstractProvider(typing.Generic[T_co], abc.ABC):
 
     @property
     def cast(self) -> T_co:
-        """Returns self, but cast to the type of the provided value.
-
-        This helps to pass providers as input to other providers while avoiding type checking errors:
-        :example:
-
-            class A: ...
-
-            def create_b(a: A) -> B: ...
-
-            class Container(BaseContainer):
-                a_factory = Factory(A)
-                b_factory1 = Factory(create_b, a_factory)  # works, but mypy (or pyright, etc.) will complain
-                b_factory2 = Factory(create_b, a_factory.cast)  # works and passes type checking
-        """
+        """Returns self, but cast to the type of the provided value.\n\n        This helps to pass providers as input to other providers while avoiding type checking errors:\n        :example:\n\n            class A: ...\n\n            def create_b(a: A) -> B: ...\n\n            class Container(BaseContainer):\n                a_factory = Factory(A)\n                b_factory1 = Factory(create_b, a_factory)  # works, but mypy (or pyright, etc.) will complain\n                b_factory2 = Factory(create_b, a_factory.cast)  # works and passes type checking\n        """
         return typing.cast(T_co, self)
 
 
@@ -73,12 +53,7 @@ class ResourceContext(typing.Generic[T_co]):
     __slots__ = "context_stack", "instance", "resolving_lock", "is_async"
 
     def __init__(self, is_async: bool) -> None:
-        """Create a new ResourceContext instance.
-
-        :param is_async: Whether the ResourceContext was created in an async context.
-        For example within a ``async with container_context(): ...`` statement.
-        :type is_async: bool
-        """
+        """Create a new ResourceContext instance.\n\n        :param is_async: Whether the ResourceContext was created in an async context.\n        For example within a ``async with container_context(): ...`` statement.\n        :type is_async: bool\n        """
         self.instance: T_co | None = None
         self.resolving_lock: typing.Final = asyncio.Lock()
         self.context_stack: contextlib.AsyncExitStack | contextlib.ExitStack | None = None
@@ -109,10 +84,7 @@ class ResourceContext(typing.Generic[T_co]):
         self.instance = None
 
     def sync_tear_down(self) -> None:
-        """Sync tear down the context stack.
-
-        :raises RuntimeError: If the context stack is async and the tear down is called in sync mode.
-        """
+        """Sync tear down the context stack.\n\n        :raises RuntimeError: If the context stack is async and the tear down is called in sync mode.\n        """
         if self.context_stack is None:
             return
 
@@ -144,6 +116,7 @@ class AbstractResource(AbstractProvider[T_co], abc.ABC):
         self._creator: typing.Final = creator
         self._args: typing.Final = args
         self._kwargs: typing.Final = kwargs
+        self._override = None
 
     def _is_creator_async(
         self, _: typing.Callable[P, typing.Iterator[T_co] | typing.AsyncIterator[T_co]]
@@ -180,12 +153,9 @@ class AbstractResource(AbstractProvider[T_co], abc.ABC):
                         T_co,
                         await context.context_stack.enter_async_context(
                             contextlib.asynccontextmanager(self._creator)(
-                                *[
-                                    await x.async_resolve() if isinstance(x, AbstractProvider) else x
-                                    for x in self._args
-                                ],
+                                *[await x() if isinstance(x, AbstractProvider) else x for x in self._args],
                                 **{
-                                    k: await v.async_resolve() if isinstance(v, AbstractProvider) else v
+                                    k: await v() if isinstance(v, AbstractProvider) else v
                                     for k, v in self._kwargs.items()
                                 },
                             ),
@@ -237,36 +207,3 @@ class AbstractFactory(AbstractProvider[T_co], abc.ABC):
     @property
     def sync_provider(self) -> typing.Callable[[], T_co]:
         return self.sync_resolve
-
-
-def _get_value_from_object_by_dotted_path(obj: typing.Any, path: str) -> typing.Any:  # noqa: ANN401
-    attribute_getter = attrgetter(path)
-    return attribute_getter(obj)
-
-
-class AttrGetter(
-    AbstractProvider[T_co],
-):
-    __slots__ = "_provider", "_attrs"
-
-    def __init__(self, provider: AbstractProvider[T_co], attr_name: str) -> None:
-        super().__init__()
-        self._provider = provider
-        self._attrs = [attr_name]
-
-    def __getattr__(self, attr: str) -> "AttrGetter[T_co]":
-        if attr.startswith("_"):
-            msg = f"'{type(self)}' object has no attribute '{attr}'"
-            raise AttributeError(msg)
-        self._attrs.append(attr)
-        return self
-
-    async def async_resolve(self) -> typing.Any:  # noqa: ANN401
-        resolved_provider_object = await self._provider.async_resolve()
-        attribute_path = ".".join(self._attrs)
-        return _get_value_from_object_by_dotted_path(resolved_provider_object, attribute_path)
-
-    def sync_resolve(self) -> typing.Any:  # noqa: ANN401
-        resolved_provider_object = self._provider.sync_resolve()
-        attribute_path = ".".join(self._attrs)
-        return _get_value_from_object_by_dotted_path(resolved_provider_object, attribute_path)
