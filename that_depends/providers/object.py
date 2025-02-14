ce69892739ee0bfaea6372661a768137bfa80dbe
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:43:02 +0000

    Reset to 1703b6501f9c9f600bfbebe389e7f63a7bf78bdd and made changes for LLM Round 1

--- that_depends/providers/object.py
+++ that_depends/providers/object.py
@@ -1,6 +1,7 @@
 import typing
 
 from that_depends.providers.base import AbstractProvider
+from that_depends.providers.singleton import Singleton
 
 
 T_co = typing.TypeVar("T_co", covariant=True)
@@ -15,9 +16,46 @@ class Object(AbstractProvider[T_co]):
         self._obj: typing.Final = obj
 
     async def async_resolve(self) -> T_co:
-        return self.sync_resolve()
+        return self._obj
 
     def sync_resolve(self) -> T_co:
-        if self._override is not None:
-            return typing.cast(T_co, self._override)
         return self._obj
+
+
+class ConfigurableObject(AbstractProvider[T_co]):
+    __slots__ = ("_factory", "_args", "_kwargs")
+
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
+        super().__init__()
+        self._factory: typing.Final = factory
+        self._args: typing.Final = args
+        self._kwargs: typing.Final = kwargs
+
+    async def async_resolve(self) -> T_co:
+        return self._factory(
+            *[await x.async_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: await v.async_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()}
+        )
+
+    def sync_resolve(self) -> T_co:
+        return self._factory(
+            *[x.sync_resolve() if isinstance(x, AbstractProvider) else x for x in self._args],
+            **{k: v.sync_resolve() if isinstance(v, AbstractProvider) else v for k, v in self._kwargs.items()}
+        )
+
+
+class SingletonObject(Singleton[T_co], AbstractProvider[T_co]):
+    __slots__ = ("_factory", "_args", "_kwargs", "_override", "_instance")
+
+    def __init__(self, factory: typing.Callable[P, T_co], *args: P.args, **kwargs: P.kwargs) -> None:
+        Singleton.__init__(self, factory, *args, **kwargs)
+        self._factory: typing.Final = factory
+        self._args: typing.Final = args
+        self._kwargs: typing.Final = kwargs
+        self._instance: T_co | None = None
+
+    async def async_resolve(self) -> T_co:
+        return await Singleton.async_resolve(self)
+
+    def sync_resolve(self) -> T_co:
+        return Singleton.sync_resolve(self)
