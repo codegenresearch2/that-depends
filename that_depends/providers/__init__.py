322f93a2df01b9ec1ce2e5cab34242c7a428ed98
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:20:05 +0000

    Reset to ad3a43fc448c1a21041ea5a8aa6f467f5e9866b8 and made changes for LLM Round 1

--- that_depends/providers/__init__.py
+++ that_depends/providers/__init__.py
@@ -1,29 +1,20 @@
 from that_depends.providers.attr_getter import AttrGetter
 from that_depends.providers.base import AbstractProvider
 from that_depends.providers.collections import Dict, List
-from that_depends.providers.context_resources import (
-    AsyncContextResource,
-    ContextResource,
-    DIContextMiddleware,
-    container_context,
-    fetch_context_item,
-    sync_container_context,
-)
+from that_depends.providers.context_resources import DIContextMiddleware, container_context
 from that_depends.providers.factories import AsyncFactory, Factory
 from that_depends.providers.object import Object
 from that_depends.providers.resources import AsyncResource, Resource
 from that_depends.providers.selector import Selector
 from that_depends.providers.singleton import Singleton
-
+from that_depends.injection import inject
+from that_depends import providers
 
 __all__ = [
     "AbstractProvider",
-    "AsyncContextResource",
     "AsyncFactory",
     "AsyncResource",
     "AttrGetter",
-    "ContextResource",
-    "DIContextMiddleware",
     "Dict",
     "Factory",
     "List",
@@ -32,6 +23,27 @@ __all__ = [
     "Selector",
     "Singleton",
     "container_context",
-    "sync_container_context",
-    "fetch_context_item",
+    "inject",
+    "providers",
 ]
+
+@inject
+class EnhancedDIContextMiddleware(DIContextMiddleware):
+    def __init__(self, *resources):
+        self.resources = resources
+
+    def __enter__(self):
+        for resource in self.resources:
+            container_context.add(resource)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for resource in self.resources:
+            container_context.remove(resource)
+
+# Example usage
+with EnhancedDIContextMiddleware(Resource1(), Resource2()):
+    # Resources are automatically managed within the context
+    pass
+
+In this rewritten code, I have added a decorator `@inject` to simplify context management. The `EnhancedDIContextMiddleware` class is created to enhance context resource functionality and flexibility. It automatically adds and removes resources from the context when entering and exiting the context, respectively. This improves code readability and maintainability by simplifying the management of resources within the context.
